17dba38ca2c7711781ab5d945e9c72db7cce7080
Adi <adi.cizmic@stu.ibu.edu.ba>
2023-10-11 14:20:53 +0200

    Week1 lab

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        // System.out.println("Adi Cizmic");
+        // System.out.println("Hello World!");
+        // System.out.print("(And all the people of the world)");
+
+        // System.out.println("There are " + (86400*365) + " in a year.");
+        // Scanner reader=new Scanner(System.in);
+        // System.out.print("Type a number: ");
+        // int first = Integer.parseInt(reader.nextLine());
+        // System.out.print("Type another number: ");
+        // int second = Integer.parseInt(reader.nextLine());
+        // int sum=first+second;
+        // System.out.println("Sum of the numbers: " + sum);
+
+        // System.out.print("Type a number: ");
+        // Scanner reader=new Scanner(System.in);
+        // int first = Integer.parseInt(reader.nextLine());
+        //if(first>0){
+        //    System.out.println("The number is positive.");
+        //}
+        //else{
+        //   System.out.println("The number is not positive.");
+        //}
+        //Scanner reader=new Scanner(System.in);
+        //System.out.println("Type the first number: ");
+        //int first=Integer.parseInt(reader.nextLine());
+        //System.out.println("Type the second number: ");
+        //int second=Integer.parseInt(reader.nextLine());
+
+        //if(first>second){
+        //    System.out.println("Greater number: " + first);
+        //}
+        //else if(second>first){
+        //    System.out.println("Greater number: " + second);
+        //}
+        //else{
+        //    System.out.println("The numbers are equal!");
+        //}
+    }
+}
